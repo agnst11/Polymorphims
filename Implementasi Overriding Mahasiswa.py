7f94cee130d1ca3c45a3ee8d54fff628f8af36e9
agnst11 <99317673+agnst11@users.noreply.github.com>
2022-03-22 21:46:57 +0700

    Add files via upload

---
+++ Implementasi Overriding Mahasiswa.py
@@ -0,0 +1,35 @@
+class Mahasiswa:
+    def __init__(self, nama, nim):
+            self.nama = nama
+            self.nim = nim
+    
+    def tampilMhs(self):
+        print("Nama : ", self.nama, ", nim :", self.nim)
+
+    # method overloading
+    def hitungSks(self, jmlsks = None, sks = None):
+        if jmlsks != None and sks != None:
+            totalsks = jmlsks + sks
+            print("Total SKS = ", totalsks)
+        else : 
+            totalsks = jmlsks
+            print("Total SKS = ", totalsks)
+
+class Mahasiswa2(Mahasiswa):
+    def __init__(self, nama, nim):
+            self.nama = nama
+            self.nim = nim
+    
+    def tampilMhs(self):
+        print(f'''
+                Detail Mahasiswa
+                Nama : {self.nama},
+                NIM  : {self.nim}
+                ''')
+
+# memanggil class
+mhs1 = Mahasiswa("Agnesta", 2000)
+mhs2 = Mahasiswa2("Maman", 6000)
+
+mhs1.tampilMhs()
+mhs2.tampilMhs()
